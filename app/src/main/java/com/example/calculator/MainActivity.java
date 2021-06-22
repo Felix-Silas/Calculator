18e8d9f402fd1e5d694e5c96bc7bfac147b46197
Felix-Silas <ygeuntae1208@naver.com>
2021-06-22 18:06:17 +0900

    second Calculator

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -6,236 +6,232 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean isfirstInput = true; //입력 값이 처음 입력되는가를 체크
-    int resultNumber = 0; // 계산된 결과 값을 저장하는 변수
-    char operator = '+'; // 입력된 연산자를 저장하는 변수
-    boolean repeatoperator= true;
+    String process;
+    boolean checkBracket = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView result = findViewById(R.id.result);
-        EditText input=findViewById(R.id.input);
-        Button zero = (Button) findViewById(R.id.zero);
-        Button one = (Button) findViewById(R.id.one);
-        Button two = (Button) findViewById(R.id.two);
-        Button three = (Button) findViewById(R.id.three);
-        Button four = (Button) findViewById(R.id.four);
-        Button five = (Button) findViewById(R.id.five);
-        Button six = (Button) findViewById(R.id.six);
-        Button seven = (Button) findViewById(R.id.seven);
-        Button eight = (Button) findViewById(R.id.eight);
-        Button nine = (Button) findViewById(R.id.nine);
-        Button clearBTN = (Button) findViewById(R.id.clearBTN);
-        Button subtractBTN = (Button) findViewById(R.id.subtractBTN);
-        Button divideBTN = (Button) findViewById(R.id.divideBTN);
-        Button multiplyBTN = (Button) findViewById(R.id.multiplyBTN);
-        Button addBTN = (Button) findViewById(R.id.addBTN);
-        Button plusMinusBTN = (Button) findViewById(R.id.plusMinusBTN);
-        Button pointBTN = (Button) findViewById(R.id.pointBTN);
-        Button equalsBTN = (Button) findViewById(R.id.equalsBTN);
-        ImageButton backspaceBTN = (ImageButton) findViewById(R.id.backspaceBTN);
+        TextView mathTextView = findViewById(R.id.mathTextView);
+        TextView resultTextView = findViewById(R.id.resultTextView);
+        Button zero = findViewById(R.id.zero);
+        Button one = findViewById(R.id.one);
+        Button two = findViewById(R.id.two);
+        Button three = findViewById(R.id.three);
+        Button four = findViewById(R.id.four);
+        Button five = findViewById(R.id.five);
+        Button six = findViewById(R.id.six);
+        Button seven = findViewById(R.id.seven);
+        Button eight = findViewById(R.id.eight);
+        Button nine = findViewById(R.id.nine);
+        Button clearBTN = findViewById(R.id.clearBTN);
+        ImageButton backspaceBTN = findViewById(R.id.backspaceBTN);
+        Button pointBTN = findViewById(R.id.pointBTN);
+        Button addBTN = findViewById(R.id.addBTN);
+        Button subtractBTN = findViewById(R.id.subtractBTN);
+        Button divideBTN = findViewById(R.id.divideBTN);
+        Button multiplyBTN = findViewById(R.id.multiplyBTN);
+        Button plusMinusBTN = findViewById(R.id.plusMinusBTN);
+        Button equalsBTN = findViewById(R.id.equalsBTN);
+        Button bracketBTN = findViewById(R.id.bracketBTN);
 
-        clearBTN.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                isfirstInput = true;
-                resultNumber = 0;
-                operator = '+';
-                input.setText(String.valueOf(resultNumber));
-            }
-        });
 
         zero.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(isfirstInput) {
-                    input.setText("0");
-                    isfirstInput=false;
-                }else{
-                    input.append("0");
-                }
+                process = mathTextView.getText().toString();
+                mathTextView.setText(process + "0");
             }
         });
 
         one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(isfirstInput) {
-                    input.setText("1");
-                    isfirstInput=false;
-                }else{
-                    input.append("1");
-                }
+                process = mathTextView.getText().toString();
+                mathTextView.setText(process + "1");
             }
         });
 
         two.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(isfirstInput) {
-                    input.setText("2");
-                    isfirstInput=false;
-                }else{
-                    input.append("2");
-                }
+                process = mathTextView.getText().toString();
+                mathTextView.setText(process + "2");
             }
         });
 
         three.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(isfirstInput) {
-                    input.setText("3");
-                    isfirstInput=false;
-                }else{
-                    input.append("3");
-                }
+                process = mathTextView.getText().toString();
+                mathTextView.setText(process + "3");
             }
         });
 
         four.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(isfirstInput) {
-                    input.setText("4");
-                    isfirstInput=false;
-                }else{
-                    input.append("4");
-                }
+                process = mathTextView.getText().toString();
+                mathTextView.setText(process + "4");
             }
         });
 
         five.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(isfirstInput) {
-                    input.setText("5");
-                    isfirstInput=false;
-                }else{
-                    input.append("5");
-                }
+                process = mathTextView.getText().toString();
+                mathTextView.setText(process + "5");
             }
         });
 
         six.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(isfirstInput) {
-                    input.setText("6");
-                    isfirstInput=false;
-                }else{
-                    input.append("6");
-                }
+                process = mathTextView.getText().toString();
+                mathTextView.setText(process + "6");
             }
         });
 
         seven.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(isfirstInput) {
-                    input.setText("7");
-                    isfirstInput=false;
-                }else{
-                    input.append("7");
-                }
+                process = mathTextView.getText().toString();
+                mathTextView.setText(process + "7");
             }
         });
 
         eight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(isfirstInput) {
-                    input.setText("8");
-                    isfirstInput=false;
-                }else{
-                    input.append("8");
-                }
+                process = mathTextView.getText().toString();
+                mathTextView.setText(process + "8");
             }
         });
 
         nine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(isfirstInput) {
-                    input.setText("9");
-                    isfirstInput=false;
-                }else{
-                    input.append("9");
-                }
+                process = mathTextView.getText().toString();
+                mathTextView.setText(process + "9");
             }
         });
 
-        addBTN.setOnClickListener(new View.OnClickListener() {
+        clearBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              if(isfirstInput==false && repeatoperator==true){
-                  input.append("+");
-                  repeatoperator = false;
-              }else{}
-                    }
-                });
-
-        subtractBTN.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(isfirstInput==false && repeatoperator==true){
-                    input.append("-");
-                    repeatoperator = false;
-                }else{}
-            }
-        });
-
-        divideBTN.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(isfirstInput==false && repeatoperator==true){
-                    input.append("÷");
-                    repeatoperator = false;
-                }else{}
-            }
-        });
-
-        multiplyBTN.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(isfirstInput==false && repeatoperator==true){
-                    input.append("×");
-                    repeatoperator = false;
-                }else{}
+                mathTextView.setText("");
+                resultTextView.setText("");
             }
         });
 
         backspaceBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(input.getText().toString().length() >1){
-                    String getInput = input.getText().toString();
-                    String subInput = getInput.substring(0,getInput.length()-1);
-                    input.setText(subInput);
-                } else {
-                    input.setText("");
+                String backspace = null;
+                if (mathTextView.getText().toString().length() > 0) {
+                    StringBuilder stringBuilder = new StringBuilder(mathTextView.getText());
+                    stringBuilder.deleteCharAt(mathTextView.getText().length() - 1);
+                    backspace = stringBuilder.toString();
+                    mathTextView.setText(backspace);
                 }
             }
         });
 
-        equalsBTN.setOnClickListener(new View.OnClickListener() {
+        pointBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                process = mathTextView.getText().toString();
+                mathTextView.setText(process + ".");
 
             }
         });
 
+        addBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = mathTextView.getText().toString();
+                mathTextView.setText(process + "+");
+            }
+        });
+
+        subtractBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = mathTextView.getText().toString();
+                mathTextView.setText(process + "-");
+            }
+        });
+
+        divideBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = mathTextView.getText().toString();
+                mathTextView.setText(process + "÷");
+            }
+        });
+
+        multiplyBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = mathTextView.getText().toString();
+                mathTextView.setText(process + "×");
+            }
+        });
+
+        bracketBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkBracket){
+                    process = mathTextView.getText().toString();
+                    mathTextView.setText(process+"(");
+                    checkBracket = false;
+                } else {
+                    process = mathTextView.getText().toString();
+                    mathTextView.setText(process+")");
+                    checkBracket = true;
+                }
+            }
+        });
+
+
+        equalsBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = mathTextView.getText().toString();
+                process = process.replaceAll("×","*");
+                process = process.replaceAll("÷","/");
+
+                Context rhino = Context.enter();
+                rhino.setOptimizationLevel(-1);
+                String finalResult = "";
+
+                try{
+                    Scriptable scriptable = rhino.initStandardObjects();
+                    finalResult= rhino.evaluateString(scriptable,process,"javascript",1,null).toString();
+                }catch (Exception e){
+                    finalResult = "0";
+                }
+                resultTextView.setText(finalResult);
+
+
+            }
+        });
+
+
     }
-
-
 }
+
+
+
+
 
